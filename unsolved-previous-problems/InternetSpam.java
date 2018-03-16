dc6df8893122b0a4448de1ad378d2bf6a1978cd3
Haya <Hayaa@taibahu.edu.sa>
2018-03-16 18:48:20 +0300

    Updated Internet Spam

--- unsolved-previous-problems/InternetSpam.java
+++ unsolved-previous-problems/InternetSpam.java
@@ -9,16 +9,18 @@ public class InternetSpam {
         while (in.hasNext()) {
             String link =  in.nextLine();
             String[] tmp = link.split("rel=\"");
+            String s;
             if(tmp.length > 1) {
                 int index = tmp[1].indexOf('"');
-                String s = (index > 0)? tmp[1].substring(0, index) + " ": "";
-                System.out.println(tmp[0] + "rel=\"" + s + "nofollow" + tmp[1].substring(index, tmp[1].length()));
+                s = (index > 0)? tmp[1].substring(0, index) + " ": "";
+                s = tmp[0] + "rel=\"" + s + "nofollow" + tmp[1].substring(index, tmp[1].length());
             } else {
                 tmp = new String[2];
                 tmp[0] = link.substring(0, link.indexOf('>'));
-                tmp[1] = "\"" + link.substring(link.indexOf('>') + 1, link.length());
-                System.out.println(tmp[0] + " rel=\"nofollow\">" + tmp[1]);
+                tmp[1] = "\"" + link.substring(link.indexOf('>'), link.length());
+                s = tmp[0] + " rel=\"nofollow" + tmp[1];
             }
+            System.out.println(s.substring(s.indexOf('<'), s.lastIndexOf('>') + 1));
         }
     }
 }
